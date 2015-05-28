eebbbd8606c6e6d1e4893e273ce05c2859f421af
Jordan Curzon <curzonj@gmail.com>
2015-05-27 20:39:54 -0700

    add gulp and jsbeautifier

---
+++ gulp/tasks/default.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+    console.log("default doesn't do anything here")
+})
---
+++ gulp/tasks/jsbeautifier.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var prettify = require('gulp-jsbeautifier');
+
+gulp.task('git-pre-js', function() {
+  gulp.src('./src/**.js')
+    .pipe(prettify({config: '.jsbeautifyrc', mode: 'VERIFY_ONLY'}))
+});
+
+gulp.task('format-js', function() {
+  gulp.src(['./src/**.js'], { base: './' })
+    .pipe(prettify({config: '.jsbeautifyrc', mode: 'VERIFY_AND_WRITE'}))
+    .pipe(gulp.dest('./'))
+});
